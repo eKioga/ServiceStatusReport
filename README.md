b7fb67c31b5f5f6300a96580f6c571411b2792a5
Eric Post <erickpost@gmail.com>
2017-06-19 11:47:14 -0700

    updated code with comments and backticks for easy readability

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ServiceStatusReport
 PowerShell script that find the status of a service and then pipes the results to an stylized HTML page.
+
+I mostly used this script to quickly rule out network or service related reasons when certain apps would start acting up.
--- ServiceStatusReport.ps1
+++ ServiceStatusReport.ps1
@@ -1,9 +1,23 @@
+# << Service Status Report >> Conact Eric Post for help.
+# Variables below are for editing the style of the HTML thats generated at the end.
 $a = "<style>"
 $a = $a + "BODY{background-color:gainsboro;}"
 $a = $a + "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}"
 $a = $a + "TH{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:limegreen}"
 $a = $a + "TD{border-width: 1px;padding: 0px;border-style: solid;border-color: black;background-color:whitesmoke}"
 $a = $a + "</style>"
+# Text to be displayed BEFORE the table. Note: the 'every five minutes' reference below is due to how often the task for this script will run.
 $Pre = "Status refreshes every five minutes."
+# Text to be displayed AFTER the table
 $Post = "Missing Application? Contact Eric Post to add more."
-icm aei-pdq {Get-Service -name PDQDeploy, PDQInventory} | ConvertTo-Html Status,DisplayName,PSComputerName, -title "AEI Critical Application Status" -body "<H2>AEI Critical Application Status</H2>" -head $a -PreContent $Pre -PostContent $Post | Set-Content C:\inetpub\wwwroot\app_status.htm
+# Replace 'servername' with the server that is hosting the services.
+# Replace 'serviceName' with the name of the service(s) you want to include on this report.
+icm servername {Get-Service -name serviceName1, serviceName2} `
+| ConvertTo-Html Status,DisplayName,PSComputerName, `
+# Edit Title and Body text if you wish 
+-title "Critical Application Status" `
+-body "<H2>Critical Application Status</H2>" `
+-head $a -PreContent $Pre -PostContent $Post `
+# Then it generates the HTML file and places it in a specific directory.
+# Note that in this case, the local server also ran an IIS service.
+| Set-Content C:\inetpub\wwwroot\app_status.htm
